7f87369f353bbf2e495bd8b6f4b7dde40c54b829
Brandon Rainey <brandonhrainey@gmail.com>
2023-06-20 23:48:29 -0400

    changed card showing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,74 @@
 import { useState, useEffect } from 'react'
-import { createClient } from "@supabase/supabase-js";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { createClient } from '@supabase/supabase-js'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './App.scss'
-import { Box, Button } from '@mui/material';
+import { Box, Button } from '@mui/material'
 
-
-const supabase = createClient("https://ucexdsdxcrgpnektgmbe.supabase.co", import.meta.env.VITE_SUPABASE_ANON);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON
+)
 
 function App() {
-  
-  const [cards, setCards] = useState<any>([])
-
-  const [cardIndex, setCardIndex] = useState(0)
+  const [card, setCard] = useState<any | null>()
 
   const [showingCard, setShowingCard] = useState(false)
 
-  
-  async function getCards() {
-    const { data } = await supabase.from("my_table").select().range(0,9);
-    setCards(data);
+  const [randomNumber, setRandomNumber] = useState(generateRandomNumber(1000))
+
+  function generateRandomNumber(maxValue: number) {
+    return Math.floor(Math.random() * (maxValue + 1))
   }
 
-  function handleButtonClick() {
-    
+  async function getCards() {
+    const { data } = await supabase
+      .from('my_table')
+      .select()
+      .range(randomNumber, randomNumber)
+      if (data !== null && data.length > 0) {
+        setCard(data[0]);
+      }
+  }
+
+  async function handleButtonClick() {
     if (showingCard) {
-      setCardIndex(cardIndex + 1)
+      await getCards()
+
       setShowingCard(false)
     } else {
       setShowingCard(true)
+      setRandomNumber(generateRandomNumber(1000))
     }
   }
 
   useEffect(() => {
-    getCards();
-  }, []);
+    getCards()
+  }, [])
 
-  console.log(cards)
+  console.log(card)
   return (
     <>
       <div>
-        <Box className='cardWrapper'>
-          <p className='cardTitle'>{cards[cardIndex]?.flds[0]}</p>
-          <p className='cardDescription'>{showingCard ? cards[cardIndex]?.flds[2] : ''}</p>
-          <Button variant="contained" className={`cardButton `} onClick={() => handleButtonClick()}>
+        <Box className="cardWrapper">
+          <p className="cardTitle">{card?.flds[0]}</p>
+          <p className="cardPronounciation">
+            {showingCard ? card?.flds[1] : ''}
+          </p>
+          <p className="cardDescription">
+            {showingCard ? card?.flds[2] : ''}
+          </p>
+          <Button
+            variant="contained"
+            className={`cardButton `}
+            onClick={() => handleButtonClick()}
+          >
             {showingCard ? 'Next' : 'Show Card'}
           </Button>
         </Box>
       </div>
-      
     </>
   )
 }
